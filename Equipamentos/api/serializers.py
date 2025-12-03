7d6c3a514418a5ceaafd9c324e98d3d7ea398c67
Wilton Silva <wiltonsilvarodrigues4321@gmail.com>
2025-12-03 15:14:19 -0300

    fix: Adiciona campo status ao EquipamentosSerializer

--- Equipamentos/api/serializers.py
+++ Equipamentos/api/serializers.py
@@ -4,4 +4,4 @@ from Equipamentos.models import Equipamento
 class EquipamentosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipamento
-        fields = ['id', 'nome', 'modelo', 'numero_serie', 'fabricante', 'data_aquisicao']
+        fields = ['id', 'nome', 'modelo', 'numero_serie', 'fabricante', 'data_aquisicao','status']
